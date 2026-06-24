53a7ce39e45b8f6728151af11ee40d426ee06b79
Vivovim <59324676+Vivovim@users.noreply.github.com>
2026-06-23 17:47:18 -0700

    Initial Commit

---
+++ Neo Files/Neo_FilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neo_FilesApp.swift
+//  Neo Files
+//
+//  Created by Christopher Huffaker on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Neo_FilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
